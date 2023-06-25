39c5bdf167d59b3d058836b84a1e2615c3fb56a3
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-06-25 17:37:07 +0530

    Cloudinary Support

--- index.js
+++ index.js
@@ -3,48 +3,54 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const jsonParser = express.json();
-app.use(jsonParser); 
-const fetch = require('node-fetch'); //npm install node-fetch@2
+app.use(jsonParser);
+const fetch = require('node-fetch');
 const https = require('https');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: '10mb' }));
+const API_KEY = process.env.API_KEY;
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-app.set('views',[path.join(__dirname, 'views'),
-        path.join(__dirname, 'views')]
-);
+app.set('views', [
+  path.join(__dirname, 'views'),
+  path.join(__dirname, 'views')
+]);
 
-app.get('/',(req,res)=>{
-    res.render('index.ejs');
+app.get('/', (req, res) => {
+  console.log(API_KEY)
+  res.render('index.ejs');
+});
+console.log(API_KEY)
+app.post('/describe', (req, res) => {
+  const imageUrl = req.body.imageUrl;
+
+  const requestData = {
+    tkn: '80DA34F4-746E-4764-83BC-E0C0AD5F65003F455633-D7B2-4294-83EC-FADF7EEB778A',
+    modelVersion: '1.0_full',
+    input: imageUrl,
+    visionParams: 'gpt'
+  };
+
+  const agent = new https.Agent({ rejectUnauthorized: false });
+  fetch('https://astica.ai:9141/vision/describe', {
+    method: 'POST',
+    body: JSON.stringify(requestData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    agent: agent,
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'An error occurred' });
+    });
 });
 
-app.post('/describe', (req, res) => {
-    const imageUrl = req.body.imageUrl;
-  
-    const requestData = {
-      tkn: '80DA34F4-746E-4764-83BC-E0C0AD5F65003F455633-D7B2-4294-83EC-FADF7EEB778A',
-      modelVersion: '1.0_full',
-      input: imageUrl,
-      visionParams: 'gpt'
-    };
-  
-    const agent = new https.Agent({ rejectUnauthorized: false });
-    fetch('https://astica.ai:9141/vision/describe', {
-      method: 'POST',
-      body: JSON.stringify(requestData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      agent: agent,
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        res.json(data);
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).json({ error: 'An error occurred' });
-      });
-  });
-
-app.listen(8000, () => {
+app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
