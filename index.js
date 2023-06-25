c8fbba2a736a75d2cf6a6a2c75aaa3fb76e944aa
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-06-25 14:33:02 +0530

    Camera Module

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000; // Change this to the desired port number
 const path = require('path');
-app.use(express.json()); 
+const jsonParser = express.json();
+app.use(jsonParser); 
+const fetch = require('node-fetch'); //npm install node-fetch@2
+const https = require('https');
 
-// Set the view engine to EJS
-app.set('view engine', 'ejs');
 app.set('views',[path.join(__dirname, 'views'),
         path.join(__dirname, 'views')]
 );
-app.use(express.static(path.join(__dirname,'public')));
 
-// Define a route handler for the home page
-app.get('/', (req, res) => {
-  // Render the EJS template
-  res.render('index.ejs');
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.post('/describe', (req, res) => {
+    const imageUrl = req.body.imageUrl;
+  
+    const requestData = {
+      tkn: '80DA34F4-746E-4764-83BC-E0C0AD5F65003F455633-D7B2-4294-83EC-FADF7EEB778A',
+      modelVersion: '1.0_full',
+      input: imageUrl,
+      visionParams: 'gpt'
+    };
+  
+    const agent = new https.Agent({ rejectUnauthorized: false });
+    fetch('https://astica.ai:9141/vision/describe', {
+      method: 'POST',
+      body: JSON.stringify(requestData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      agent: agent,
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        res.json(data);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: 'An error occurred' });
+      });
+  });
+
+app.listen(8000, () => {
+  console.log('Server is running on port 3000');
 });
