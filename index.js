2ddbede19c1699a7b43c9fe1255e1038191e7ac3
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-06-25 21:25:34 +0530

    Vercel Deploy

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const port = 3000; // Change this to the desired port number
 const path = require('path');
-const jsonParser = express.json();
-app.use(jsonParser);
-const fetch = require('node-fetch');
-const https = require('https');
-const bodyParser = require('body-parser');
-app.use(bodyParser.json({ limit: '10mb' }));
 const API_KEY = process.env.API_KEY;
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
+const fs = require('fs');
 
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
 app.set('views', [
   path.join(__dirname, 'views'),
   path.join(__dirname, 'views')
 ]);
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Define a route handler for the home page
 app.get('/', (req, res) => {
-  console.log(API_KEY)
+  // Render the EJS template
   res.render('index.ejs');
 });
-console.log(API_KEY)
-app.post('/describe', (req, res) => {
-  const imageUrl = req.body.imageUrl;
 
-  const requestData = {
-    tkn: '80DA34F4-746E-4764-83BC-E0C0AD5F65003F455633-D7B2-4294-83EC-FADF7EEB778A',
-    modelVersion: '1.0_full',
-    input: imageUrl,
-    visionParams: 'gpt'
-  };
+// Set up Multer for handling file uploads
+const upload = multer({ dest: 'uploads/' });
 
-  const agent = new https.Agent({ rejectUnauthorized: false });
-  fetch('https://astica.ai:9141/vision/describe', {
-    method: 'POST',
-    body: JSON.stringify(requestData),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    agent: agent,
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      res.json(data);
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json({ error: 'An error occurred' });
+// POST route for file upload
+app.post('/upload', upload.single('image'), async (req, res) => {
+  // Check if file was uploaded successfully
+  if (!req.file) {
+    res.status(400).json({ error: 'No file uploaded' });
+    return;
+  }
+
+  try {
+    // Upload the file to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path);
+    const imageUrl = result.secure_url;
+
+    // Delete the uploaded file from the local filesystem
+    fs.unlink(req.file.path, (error) => {
+      if (error) {
+        console.error(error);
+      }
     });
+
+    res.json({ imageUrl });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred during file upload' });
+  }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
